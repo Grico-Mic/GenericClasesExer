2ad6b67bb659285d467cbf3afccf8c17deb32652
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-23 20:30:32 +0100

    Add folder Models and classes with properties

---
+++ GenericClasesExer/Models/Classroom.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace GenericClasesExer.Models
+{
+    public class Classroom
+    {
+        public int Id { get; set; }
+        public int Capacity { get; set; }
+        public List<string> ApplicableSubject { get; set; }
+
+
+    }
+}
---
+++ GenericClasesExer/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace GenericClasesExer.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<string> Subjects { get; set; }
+
+
+    }
+}
