c059cc09124293816b0876d90f21fba7487996b6
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-23 20:56:52 +0100

    Add folder Repositories with class StudentRepository and add method for create.

---
+++ GenericClasesExer/Repositories/StudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using GenericClasesExer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClasesExer.Repositories
+{
+    public class StudentRepository
+    {
+        public StudentRepository()
+        {
+            Students = new List<Student>();
+        }
+        private List<Student> Students;
+
+        public void Create(Student student)
+        {
+            Students.Add(student);
+        }
+    }
+}
