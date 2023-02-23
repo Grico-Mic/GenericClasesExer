9e144a2152b4e62e0a75bd16166c867e2515a8eb
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-23 20:39:50 +0100

    Create objects for students.

--- GenericClasesExer/Program.cs
+++ GenericClasesExer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using GenericClasesExer.Models;
+using System;
+using System.Collections.Generic;
 
 namespace GenericClasesExer
 {
@@ -6,7 +8,20 @@ namespace GenericClasesExer
     {
         static void Main(string[] args)
         {
-           
+            var student1 = new Student();
+            student1.Id = 1;
+            student1.Name = "Chuck";
+            student1.Surname = "Noris";
+            student1.Subjects = new List<string>() { "Math","History"};
+
+            var student2 = new Student()
+            {
+                Id = 2,
+                Name = "Bruce",
+                Surname = "Lee",
+                Subjects = new List<string>() { "Muzic","Art"}
+            };
+
         }
     }
 }
