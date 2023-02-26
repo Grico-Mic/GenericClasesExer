ade07feaec33033bd72c297281ed0dfb8ab9e680
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-26 19:13:21 +0100

    Create repository for Classroom with method's for Create,Delete and GetAll.

---
+++ GenericClasesExer/Repositories/ClassroomRepository.cs
@@ -0,0 +1,31 @@
+﻿using GenericClasesExer.Models;
+using System.Collections.Generic;
+
+
+namespace GenericClasesExer.Repositories
+{
+   public  class ClassroomRepository
+    {
+        public ClassroomRepository()
+        {
+            Classroom = new List<Classroom>();
+        }
+
+        private List<Classroom> Classroom;
+
+        public void Create(Classroom classroom)
+        {
+            Classroom.Add(classroom);
+        }
+
+        public void Delete(Classroom classroom)
+        {
+            Classroom.Remove(classroom);
+        }
+
+        public List<Classroom> GetAll()
+        {
+            return Classroom;
+        }
+    }
+}
