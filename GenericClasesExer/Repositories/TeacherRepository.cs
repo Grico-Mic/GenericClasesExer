daa8581d1e5b11f05f9686c95b1f7e22b93f62e3
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-26 19:47:31 +0100

    Create TeacherRepository with constructor and method's Create,Delete and GetAll.

---
+++ GenericClasesExer/Repositories/TeacherRepository.cs
@@ -0,0 +1,32 @@
+﻿using GenericClasesExer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericClasesExer.Repositories
+{
+    public class TeacherRepository
+    {
+        public TeacherRepository()
+        {
+            Teacher = new List<Teacher>();
+        }
+
+        private List<Teacher> Teacher;
+
+        internal void Create(Teacher teacher)
+        {
+            Teacher.Add(teacher);
+        }
+
+        internal void Delete(Teacher teacher)
+        {
+            Teacher.Remove(teacher);
+        }
+
+        public List<Teacher> GetAll()
+        {
+            return Teacher;
+        }
+    }    
+}
