754c5ff6f389a93579c316c08584264ed8baca98
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-26 19:18:57 +0100

    Create Class Teacher with properties and List

---
+++ GenericClasesExer/Models/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace GenericClasesExer.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public List<string> SubjectsTeaching { get; set; }
+
+    }
+}
