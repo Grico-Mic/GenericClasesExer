b2c32aa0d3351d9d58cf9ac507bcddab64963eec
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-27 18:31:58 +0100

    Create specific method only for this repository named "GetStudentSubjects".

--- GenericClasesExer/Repositories/StudentRepository.cs
+++ GenericClasesExer/Repositories/StudentRepository.cs
@@ -1,13 +1,18 @@
 ﻿using GenericClasesExer.Models;
-
-
+using System.Collections.Generic;
 
 namespace GenericClasesExer.Repositories
 {
     public class StudentRepository : BaseRepository<Student>
     {
+        public List<string> GetStudentSubjects(int id)
+        {
+            var student = GetById(id);
+            return student.Subjects;
+        }
     }
 }
 
 
 
+ 
